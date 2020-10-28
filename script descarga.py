c452022431ac97aa3a4ea91b07ef9fad69706f10
icorrea2013 <51419218+icorrea2013@users.noreply.github.com>
2020-10-27 22:35:43 -0300

    Add files via upload
    Avance Script de descarga

--- script descarga.py
+++ script descarga.py
@@ -1,20 +1,26 @@
-import csv;import requests;import pandas as pd;import sys;import os;import datetime;import time as ti;
-
+import csv;import requests;import pandas as pd;import sys;import os;import datetime;import time as ti
 
 #Borrado de carpeta
 from os import listdir
-test=os.listdir("C:/Users/icorr/Downloads/TALLER_IV/TALLER IV") #cambiar por ruta local
+test=os.listdir("C:/Users/icorr/Desktop/TALLER IV") #cambiar por ruta local
 for item in test:
     if item.endswith(".csv"):
         os.remove(item)
 
 #descarga csv
-Url='https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto4/2020-10-19-CasosConfirmados-totalRegional.csv'
+a = 0
+dias = 0
+for a in range(5):
+    fecha = datetime.date.today()
+    n = fecha - datetime.timedelta(days=dias)
+    dias = dias + 1
+    F_string = n.strftime('%y%y-%m-%d')
 
-req = requests.get(Url)
-
-url_content = req.content
-csv_file = open('downloaded.csv', 'wb')
-csv_file.write(url_content)
-csv_file.close()
+    Url='https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto4/'+F_string+'-CasosConfirmados-totalRegional.csv'
+    req = requests.get(Url)
+    url_content = req.content
+    csv_file = open(F_string+'.csv', 'wb')
+    csv_file.write(url_content)
+    csv_file.close()
+    a = a + 1
 
